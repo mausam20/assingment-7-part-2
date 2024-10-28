0a2611e1e2fe717e02ee3d05fc1868721b5cb5e4
mausam20 <20aasthaprasad@gmail.com>
2024-10-27 20:41:56 -0700

    Update app.py
    removed extra line

--- app.py
+++ app.py
@@ -2,32 +2,26 @@ from fastapi import FastAPI
 from sqlmodel import Field, Session, SQLModel, create_engine, select
 from config import settings
 
-
 class Hero(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
     name: str = Field(index=True)
     secret_name: str
     age: int | None = Field(default=None, index=True)
 
-
 engine = create_engine(str(settings.SQLALCHEMY_DATABASE_URI))
-
 
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
 
 app = FastAPI()
 
-
 @app.on_event("startup")
 def on_startup():
     create_db_and_tables()
 
-
 @app.get("/")
 def hello():
     return "Hello, Docker!"
-
 
 @app.post("/heroes/")
 def create_hero(hero: Hero):
@@ -36,7 +30,6 @@ def create_hero(hero: Hero):
         session.commit()
         session.refresh(hero)
         return hero
-
 
 @app.get("/heroes/")
 def read_heroes():
